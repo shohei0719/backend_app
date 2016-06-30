00a59a55747dad1a257a72bb498ae94e207979fc
野口翔平 <ns.fresnel07@gmail.com>
2016-07-01 07:44:59 +0900

    test

---
+++ app/library/MyTags.php
@@ -0,0 +1,11 @@
+<?php
+
+use Phalcon\Tag;
+
+class MyTags extends Tag
+{
+  static public function errorMsg($message)
+  {
+    return '<div class="alert alert-danger">' . $message . '</div>';
+  }
+}
